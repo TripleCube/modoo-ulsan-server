93c73a9f0f12d9e46d18b2064bac08c00e9bf6b8
TripleCube <can@live.in>
2020-06-08 16:03:04 +0900

    Add configuration for Sequelize

---
+++ src/config/database.js
@@ -0,0 +1,34 @@
+import env from './env';
+
+const database = {
+  database: env.database.name,
+  username: env.database.username,
+  password: env.database.password,
+  host: env.database.host,
+  port: env.database.port,
+  dialect: 'mariadb',
+  dialectOptions: {
+    dateStrings: true,
+    timezone: env.database.timezone,
+  },
+  define: {
+    charset: 'utf8mb4',
+    collate: 'utf8mb4_unicode_ci',
+    timestamps: true,
+    underscored: true,
+    freezeTableName: true,
+  },
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+  logging: env.database.logging,
+};
+
+module.exports = {
+  [env.node]: database,
+};
+
+export { database as default };
---
+++ src/config/env.js
@@ -0,0 +1,41 @@
+import dotenv from 'dotenv';
+
+const result = dotenv.config();
+
+if (result.error) {
+  throw new Error("Couldn't find env file");
+}
+
+let name;
+let logging;
+
+switch (process.env.NODE_ENV) {
+  case 'production':
+    name = process.env.DB_PROD;
+    logging = false;
+    break;
+  case 'test':
+    name = process.env.DB_TEST;
+    logging = false;
+    break;
+  default:
+    process.env.NODE_ENV = 'development';
+    name = process.env.DB_DEV;
+    logging = console.log;
+}
+
+export default {
+  node: process.env.NODE_ENV,
+  server: {
+    port: process.env.PORT || 3000,
+  },
+  database: {
+    name,
+    logging,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT, 10),
+    timezone: process.env.DB_TIMEZONE,
+  },
+};
