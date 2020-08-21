b7af1f62026feaf9706f636df089a8efbe3fb632
Tricube <can@live.in>
2020-08-21 20:32:00 +0900

    Move validator constraints to constraint.js

--- src/api/middlewares/index.js
+++ src/api/middlewares/index.js
@@ -1,2 +1,2 @@
-export { authentication, validateBody, validateParam } from './validator';
+export { validateParams, validateBody, validateQuery } from './validator';
 export { handleClientError, handleServerError } from './error-handler';
--- src/api/middlewares/validator.js
+++ src/api/middlewares/validator.js
@@ -1,26 +1,20 @@
-import Joi from '@hapi/joi';
-
 import { ValidationError } from '@utils/errors';
 
 function validateValue(schema, value, next) {
   const result = schema.validate(value);
-  if (result.error) {
-    throw new ValidationError(result.error.details[0].message);
-  }
+  if (result.error) throw new ValidationError(result.error.details[0].message);
+
   return next();
 }
 
-function validateBody(schema) {
-  return (req, res, next) => validateValue(schema, req.body, next);
-}
-
-function validateParam(schema) {
+export function validateParams(schema) {
   return (req, res, next) => validateValue(schema, req.params, next);
 }
 
-const authentication = Joi.object({
-  email: Joi.string().email().required(),
-  password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
-});
+export function validateBody(schema) {
+  return (req, res, next) => validateValue(schema, req.body, next);
+}
 
-export { validateBody, validateParam, authentication };
+export function validateQuery(schema) {
+  return (req, res, next) => validateValue(schema, req.query, next);
+}
